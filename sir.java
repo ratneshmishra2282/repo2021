36d15d27142a73155353e19e2e811708c522dbef
Prashant0407 <76028565+Prashant0407@users.noreply.github.com>
2021-10-31 22:37:09 +0530

    Create sir.java

---
+++ sir.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class white
+{
+    public static void main ()
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter a number");
+        int p=sc.nextInt();
+       for(int i=1;i<=p;i++)
+       {
+           int a=i*i;           
+          System.out.println(a);          
+       }
+    }
+}
