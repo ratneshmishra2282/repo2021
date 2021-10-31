e48d2129bca2d9d9ab6a8349a2290605d7375adb
Prashant0407 <76028565+Prashant0407@users.noreply.github.com>
2021-10-31 14:13:41 +0530

    Create message.java

---
+++ message.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class message
+{
+    public static void main()
+    {
+        int a=10;
+        int b=5;
+        int c=a+b;
+         System.out.println("hello"+c);
+         System.out.print("students");
+    }
+}
